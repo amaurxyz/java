b8009da88841d0d99857b3a9f6c9f0c2ff776f8d
amaurxyz <129287512+amaurxyz@users.noreply.github.com>
2025-05-20 21:11:08 -0300

    Update Jogo.java

--- jogo21/spolbp2_cards-main/src/br/edu/ifsp/spo/java/cards/nucleo/Jogo.java
+++ jogo21/spolbp2_cards-main/src/br/edu/ifsp/spo/java/cards/nucleo/Jogo.java
@@ -3,70 +3,101 @@ package br.edu.ifsp.spo.java.cards.nucleo;
 import br.edu.ifsp.spo.java.cards.itens.Baralho;
 import br.edu.ifsp.spo.java.cards.regras.Pontuador;
 import br.edu.ifsp.spo.java.cards.ui.JogoUI;
+
 import java.util.Scanner;
+
 public class Jogo {
 
     private Baralho baralho;
     private Jogador jogador1;
     private Jogador jogador2;
-
     private Pontuador pontuador;
-
     private JogoUI ui;
+    private int vitoriasJogador1 = 0;
+    private int vitoriasJogador2 = 0;
+    private int rodadasParaVencer;
 
-    public Jogo(){
+    public Jogo() {
         this.ui = new JogoUI();
-
         this.pontuador = this.ui.escolherPontuador();
+        Scanner sc = new Scanner(System.in);
 
-        this.baralho = new Baralho();
         this.jogador1 = new Jogador(ui.solicitarNomeJogador(1));
         this.jogador2 = new Jogador(ui.solicitarNomeJogador(2));
 
-        for(int i = 0; i < 2; i++){
-            this.jogador1.receberCarta(this.baralho.tirarCarta());
-            this.jogador2.receberCarta(this.baralho.tirarCarta());
+        System.out.print("Digite o número de rodadas necessárias para vencer: ");
+        this.rodadasParaVencer = sc.nextInt();
+        sc.nextLine(); // consumir quebra de linha
+
+        iniciarRodadas();
+    }
+
+    private void iniciarRodadas() {
+        while (vitoriasJogador1 < rodadasParaVencer && vitoriasJogador2 < rodadasParaVencer) {
+            this.baralho = new Baralho();
+            jogador1.getMao().clear();
+            jogador2.getMao().clear();
+
+            for (int i = 0; i < 2; i++) {
+                jogador1.receberCarta(baralho.tirarCarta());
+                jogador2.receberCarta(baralho.tirarCarta());
+            }
+
+            String resultadoRodada = jogarRodada();
+            System.out.println(resultadoRodada);
+
+            if (resultadoRodada.contains(jogador1.getNome())) {
+                vitoriasJogador1++;
+            } else if (resultadoRodada.contains(jogador2.getNome())) {
+                vitoriasJogador2++;
+            }
+
+            System.out.println("\nPlacar parcial:");
+            System.out.println(jogador1.getNome() + ": " + vitoriasJogador1 + " vitória(s)");
+            System.out.println(jogador2.getNome() + ": " + vitoriasJogador2 + " vitória(s)");
+        }
+
+        System.out.println("\n=== Fim do jogo ===");
+        if (vitoriasJogador1 > vitoriasJogador2) {
+            System.out.println("🏆 " + jogador1.getNome() + " venceu o jogo!");
+        } else {
+            System.out.println("🏆 " + jogador2.getNome() + " venceu o jogo!");
         }
     }
 
-    public String Jogar(int pontuacao, Jogador jogador) {
+    private String jogarRodada() {
+        while (true) {
+            System.out.print("\nJogadores: ");
+            System.out.print(jogador1);
+            System.out.print("\nA pontuação do jogador 1 é: " + pontuador.verificarPontuacao(jogador1.getMao()));
+            System.out.print(jogador2);
+            System.out.print("\nA pontuação do jogador 2 é: " + pontuador.verificarPontuacao(jogador2.getMao()));
+
+            String estouro = pontuador.estourou(jogador1, jogador2);
+            if (!estouro.equals("Erro")) {
+                return estouro;
+            }
+
+            String n1 = jogarTurno(pontuador.verificarPontuacao(jogador1.getMao()), jogador1);
+            String n2 = jogarTurno(pontuador.verificarPontuacao(jogador2.getMao()), jogador2);
+
+            if (n1.equalsIgnoreCase("N") && n2.equalsIgnoreCase("N")) {
+                return pontuador.verificarResultado(jogador1, jogador2);
+            }
+        }
+    }
+
+    private String jogarTurno(int pontuacao, Jogador jogador) {
         Scanner sc = new Scanner(System.in);
         String letra = "N";
         if (pontuacao < 21) {
-            System.out.print(" \n Deseja pegar outra carta "+jogador.getNome()+"? S/N ");
+            System.out.print("\nDeseja pegar outra carta " + jogador.getNome() + "? S/N ");
             String verificar = sc.nextLine();
-
             if (verificar.equalsIgnoreCase("S")) {
-                jogador.receberCarta(this.baralho.tirarCarta());
+                jogador.receberCarta(baralho.tirarCarta());
                 letra = verificar;
             }
         }
         return letra;
     }
-
-    @Override
-    public String toString() {
-        String resultado = "Jogo de Baralho Genérico";
-
-        while (true){
-        System.out.print("\n Jogadores: ");
-        System.out.print( this.jogador1.toString());
-        System.out.print( "\n A pontuação do jogador 1 é: " + this.pontuador.verificarPontuacao(this.jogador1.getMao()));
-        System.out.print( this.jogador2.toString());
-        System.out.print( "\n A pontuação do jogador 2 é: " + this.pontuador.verificarPontuacao(this.jogador2.getMao()));
-
-        String estouro = this.pontuador.estourou(jogador1, jogador2);
-        if (estouro != "Erro"){
-            return estouro;
-        }
-
-        String n1= Jogar(this.pontuador.verificarPontuacao(this.jogador1.getMao()),jogador1);
-        String n2= Jogar(this.pontuador.verificarPontuacao(this.jogador2.getMao()),jogador2);
-
-        if (n1.equals("N") && n2.equals("N")){
-            resultado = this.pontuador.verificarResultado(jogador1 , jogador2 ) ;
-            return resultado;
-        }
-    }
-}}
-
+}
